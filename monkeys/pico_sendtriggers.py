c5b3fa3ad7961585591cef32d60d33b428f0c8a7
Baptiste <baptiste.caziot@gmail.com>
2022-12-09 17:30:54 +0100

    fixed pico script

--- monkeys/pico_sendtriggers.py
+++ monkeys/pico_sendtriggers.py
@@ -1,26 +1,14 @@
 
-import select
-import sys
-import time
+# To be renamed main.py
+
 from machine import Pin
 
 led = Pin(15, Pin.OUT)
 led.value(0)
 
-serial = select.poll() #new select object
-serial.register(sys.stdin) #register the stdin
+def on():
+    led.value(1)
 
-while True:
-    time.sleep(0.1)
-    if serial.poll(0):
-        while serial.poll(0):
-            res = sys.stdin.read(1)
-            print(res)
-        if res=='S':
-            print('Starting')
-            led.value(1)
-        elif res=='E':
-            print('Ending')
-            led.value(0)
-        else:
-            sys.stdout.write(res)
+def off():
+    led.value(0)
+    
